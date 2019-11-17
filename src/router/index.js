11e122ff88938fb1bad7e3b987d5c7ae902a920f
Mauro Perez <mmauroperez@gmail.com>
2019-11-17 12:27:39 -0300

    pre app deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Mantenimiento from '../views/Mantenimiento.vue';
+
 
 Vue.use(VueRouter)
 
@@ -13,10 +15,7 @@ const routes = [
   {
     path: '/mantenimiento',
     name: 'mantenimiento',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/Mantenimiento.vue')
+    component: Mantenimiento
   }
 ]
 
