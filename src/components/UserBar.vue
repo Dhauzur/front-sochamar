1e088040251f28230921712b0b2fb65d17b8c39e
JavierCollipal <jcollipal1212@gmail.com>
2020-01-31 17:25:44 -0300

    base templates for login,register and user bar added, its time to work with auth store

---
+++ src/components/UserBar.vue
@@ -0,0 +1,9 @@
+<template lang="html">
+	<h1>user bar works</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<template lang="html">
+	<h1>login template works</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped></style>
