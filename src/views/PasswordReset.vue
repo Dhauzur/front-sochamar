3ec5a7f821603da4c45852b71d8c6d99c1ede872
Javier Collipal <jcollipal1212@gmail.com>
2020-02-11 17:21:50 -0300

    style refactor on PasswordReset works

--- src/views/PasswordReset.vue
+++ src/views/PasswordReset.vue
@@ -1,46 +1,42 @@
 <template lang="html">
 	<b-container>
-		<b-row class="justify-content-center">
-			<b-col md="5">
-				<b-card>
-					<b-card-title>Recuperar contraseña</b-card-title>
-					<b-card-body>
-						<b-form @submit.prevent="sendNewPassword">
-							<!--CONTRASEÑA-->
-							<b-form-group
-								id="input-group-1"
-								label="Nueva Contraseña:"
-								label-for="email-input"
-							>
-								<b-form-input
-									id="email-input"
-									v-model.trim="formData.newPassword"
-									type="password"
-									required
-									placeholder="Ingresa la contraseña"
-								></b-form-input>
-							</b-form-group>
-							<!--NUEVA CONTRASEÑA-->
-							<b-form-group
-								id="input-group-2"
-								label="Ingresala nuevamente:"
-								label-for="email-input"
-							>
-								<b-form-input
-									id="email-input"
-									v-model.trim="formData.repeatedPassword"
-									type="password"
-									required
-									placeholder="Ingresa la contraseña nuevamente"
-								></b-form-input>
-							</b-form-group>
-							<!--SUBMIT-->
-							<b-button v-if="!loading" type="submit" variant="primary"
-								>Actualizar</b-button
-							>
-						</b-form>
-					</b-card-body>
-				</b-card>
+		<b-row id="nav" class="justify-content-center">
+			<b-col md="8" lg="6" class="background-module pb-3 px-4">
+				<h3 class="my-4">Recuperar contraseña</h3>
+				<b-form @submit.prevent="sendNewPassword">
+					<!--CONTRASEÑA-->
+					<b-form-group
+						id="input-group-1"
+						label="Nueva Contraseña:"
+						label-for="email-input"
+					>
+						<b-form-input
+							id="email-input"
+							v-model.trim="formData.newPassword"
+							type="password"
+							required
+							placeholder="Ingresa la contraseña"
+						></b-form-input>
+					</b-form-group>
+					<!--NUEVA CONTRASEÑA-->
+					<b-form-group
+						id="input-group-2"
+						label="Ingresala nuevamente:"
+						label-for="email-input"
+					>
+						<b-form-input
+							id="email-input"
+							v-model.trim="formData.repeatedPassword"
+							type="password"
+							required
+							placeholder="Ingresa la contraseña nuevamente"
+						></b-form-input>
+					</b-form-group>
+					<!--SUBMIT-->
+					<b-button v-if="!loading" class="mt-2" type="submit" variant="primary"
+						>Actualizar</b-button
+					>
+				</b-form>
 			</b-col>
 		</b-row>
 	</b-container>
@@ -100,11 +96,4 @@ export default {
 };
 </script>
 
-<style lang="css" scoped>
-.test {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
-</style>
+<style lang="css" scoped></style>
