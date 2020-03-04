bd56841c4b15086bf24a2161fd5f3419bb1e5738
JavierCollipal <jcollipal1212@gmail.com>
2020-03-04 17:47:58 -0300

    env variable fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,3 @@
 export const api = process.env.VUE_APP_URL;
 export const api_absolute = process.env.VUE_APP_URL_ABSOLUTE;
-export const google_auth_url =
-	process.env.API_GOOGLE_AUTH_URL || 'http://localhost:3000/api/v1/auth/google';
+export const google_auth_url = process.env.VUE_APP_GOOGLE_AUTH_URL;
