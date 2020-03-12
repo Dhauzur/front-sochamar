ff103e996f2f489bac1573f05f3877a508910ae6
Jesus Garcia <fairplaay16@gmail.com>
2020-03-12 12:08:55 -0400

    remove old skelenton

--- src/App.vue
+++ src/App.vue
@@ -59,47 +59,5 @@ export default {
 @import '../node_modules/vue2vis/dist/vue2vis.css';
 </style>
 <style lang="scss">
-@import '../node_modules/placeholder-loading/src/scss/placeholder-loading';
 @import url('https://fonts.googleapis.com/css?family=Kanit|Poppins|Righteous&display=swap');
-#app {
-	font-family: 'Poppins', sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	overflow-x: hidden;
-	background-color: #ebecee;
-	height: 100vh;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-	transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-	opacity: 0;
-}
-
-/* width */
-::-webkit-scrollbar {
-	width: 4px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-	background: transparent;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-	background: transparent;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-	background: transparent;
-}
-
-.pointer {
-	cursor: pointer;
-}
 </style>
