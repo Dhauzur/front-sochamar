ac89ee8637a22d764ba67240a42782a268a4c9a9
Jesus Garcia <fairplaay16@gmail.com>
2020-03-12 17:18:49 -0400

    fix width

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 	<v-container class="fill-height" fluid>
 		<v-row align="center" justify="center">
-			<v-col v-if="!passwordRecover" cols="12" sm="8" md="4" lg="3">
+			<v-col v-if="!passwordRecover" cols="12" sm="8" md="4">
 				<v-card outlined light style="position: relative">
 					<!-- reemplazar imagen por el logo final -->
 					<v-img src="@/assets/example.jpeg" height="190px"></v-img>
