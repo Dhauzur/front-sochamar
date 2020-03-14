222993131bde4dfef3a9c47a622a14a51b5ce345
Jesus Garcia <fairplaay16@gmail.com>
2020-03-14 13:21:32 -0400

    fix authtentication

--- src/App.vue
+++ src/App.vue
@@ -7,50 +7,11 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations, mapActions } from 'vuex';
-
 export default {
-	data() {
-		return {
-			oauthJWT: '',
-			drawer: true,
-		};
-	},
 	computed: {
-		profileAvatar() {
-			return this.profile.img;
-		},
-		fullName() {
-			return `${this.profile.name} ${this.profile.lastName}`;
-		},
 		layout() {
 			return this.$route.meta.layout || 'default';
 		},
-		...mapGetters({
-			isLogged: 'Auth/isLogged',
-			profile: 'User/profile',
-		}),
-	},
-	created() {
-		this.oauthJWT = this.$route.query.token;
-		if (this.oauthJWT) {
-			this.deleteQueryFromRoute();
-			this.setToken(this.oauthJWT);
-			this.fetchProfile();
-			this.oauthJWT = '';
-		} else {
-			if (this.isLogged) {
-				this.setToken(localStorage.getItem('token'));
-				this.fetchProfile();
-			}
-		}
-	},
-	methods: {
-		deleteQueryFromRoute() {
-			this.$router.replace({ query: null });
-		},
-		...mapMutations({ setToken: 'Auth/setToken', logout: 'Auth/logout' }),
-		...mapActions({ fetchProfile: 'User/fetchProfile' }),
 	},
 };
 </script>
